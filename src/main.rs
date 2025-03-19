56ffc9aac983d7549375a29ffd24cee47f3d4a2c
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2025-03-19 11:22:58 -0400

    now fetch prot form env and dotfile

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use std::{env, fs, io::Result};
 
 #[main]
 async fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
+
+    let port = env::var("PORT")
+        .map_or(Ok(8080), |v| v.parse())
+        .unwrap_or(8080);
+
     // Create a directory to serve files from
     let dir = env::current_dir()?.join("share");
     fs::create_dir_all(&dir)?;
 
     HttpServer::new(move || App::new().service(Files::new("/", &dir).show_files_listing()))
-        .bind(("0.0.0.0", 8080))?
+        .bind(("0.0.0.0", port))?
         .run()
         .await
 }
